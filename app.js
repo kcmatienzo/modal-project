89c1ae51568badf6f1a5486611da86fea343e6ae
Kryselle Matienzo <krysellematienzo@gmail.com>
2023-08-21 23:47:36 -0400

    Initialized files, added functionality

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+const overlay = document.querySelector(".overlay");
+const btnsCloseModal = document.querySelector(".close-modal");
+
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", showModal);
+}
+
+btnsCloseModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
