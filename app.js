fcbfc66fa577bc221df69bfc3739f47c67140436
Kryselle Matienzo <krysellematienzo@gmail.com>
2023-08-23 17:09:56 -0400

    refactor and added functionality on other buttons

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 "use strict";
 
-const modal = document.querySelector(".modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 const overlay = document.querySelector(".overlay");
-const btnsCloseModal = document.querySelector(".close-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 
-const showModal = function () {
-  modal.classList.remove("hidden");
+const modals = document.querySelectorAll(".modal");
+const btnsCloseModal = document.querySelectorAll(".close-modal");
+
+function showModal(i) {
+  modals[i].classList.remove("hidden");
   overlay.classList.remove("hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", showModal);
 }
 
-btnsCloseModal.addEventListener("click", closeModal);
+function closeModal(j) {
+  modals[j].classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", function () {
+    showModal(i);
+  });
+}
+
+for (let j = 0; j < btnsCloseModal.length; j++) {
+  btnsCloseModal[j].addEventListener("click", function () {
+    closeModal(j);
+  });
+}
 
 document.addEventListener("keydown", function (e) {
-  console.log(e.key);
-
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
+  for (let k = 0; k < modals.length; k++) {
+    if (e.key === "Escape" && !modals[k].classList.contains("hidden")) {
+      closeModal(k);
+    }
   }
 });
+
+/*
+ MUST FIX ON NEXT UPDATE:
+ 1. After opening a few modals, the previous ones does not close automatically.
+ */
